59ec22eeaf304c932bf485375eb06abd461de18e
Michel Victor <michelvictor.doc@gmail.com>
2021-04-08 12:01:48 -0300

    updating for heroku

--- config/prod.secret.exs
+++ config/prod.secret.exs
@@ -12,7 +12,7 @@ database_url =
     """
 
 config :exticket, Exticket.Repo,
-  # ssl: true,
+  ssl: true,
   url: database_url,
   pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10")
 
